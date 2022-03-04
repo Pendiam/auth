3c6167e1d02df17dee4a304cef457bf73047795b
Pendiam <peter.arnold254@gmail.com>
2022-03-04 12:12:11 +0300

    errors

--- public/register.php
+++ public/register.php
@@ -1,3 +1,9 @@
+<?php
+require __DIR__ . '/../src/register.php';
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,34 +17,41 @@
 
 <body>
     <main>
-    <form action="register.php" method="post">
-        <h1>Sign Up</h1>
-        <div>
-            <label for="username">Username:</label>
-            <input type="text" name="username" id="username">
-        </div>
-        <div>
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email">
-        </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <div>
-            <label for="password2">Password Again:</label>
-            <input type="password" name="password2" id="password2">
-        </div>
-        <div>
-            <label for="agree">
-                <input type="checkbox" name="agree" id="agree" value="yes"/> I agree
-                with the
-                <a href="#" title="term of services">term of services</a>
-            </label>
-        </div>
-        <button type="submit">Register</button>
-        <footer>Already a member? <a href="login.php">Login here</a></footer>
-    </form>
+        <form action="register.php" method="post">
+            <h1>Sign Up</h1>
+            <div>
+                <?php
+                if (isset($errors[0])) {
+                    foreach ($errors[0] as $unameErrors) {
+                        echo "<p class='alert-error'>" . $unameErrors . "</p>";
+                    }
+                }
+                ?>
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div>
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email">
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div>
+                <label for="password2">Password Again:</label>
+                <input type="password" name="password2" id="password2">
+            </div>
+            <div>
+                <label for="agree">
+                    <input type="checkbox" name="agree" id="agree" value="yes" /> I agree
+                    with the
+                    <a href="#" title="term of services">term of services</a>
+                </label>
+            </div>
+            <button type="submit" name="btnRegister">Register</button>
+            <footer>Already a member? <a href="login.php">Login here</a></footer>
+        </form>
     </main>
 </body>
 
